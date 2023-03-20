e20aa39491eaff88fa111c74fe16663f4fec086d
Siddardha <siddardhadevarayapalli@gmail.com>
2023-03-20 19:01:31 +0530

    added notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,65 @@
+import 'dart:convert';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  await setupFlutterNotifications();
   runApp(const MyApp());
+}
+late AndroidNotificationChannel channel;
+
+bool isFlutterLocalNotificationsInitialized = false;
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+Future<void> setupFlutterNotifications() async {
+  if (isFlutterLocalNotificationsInitialized) {
+    return;
+  }
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  /// Create an Android Notification Channel.
+  ///
+  /// We use this channel in the `AndroidManifest.xml` file to override the
+  /// default FCM channel to enable heads up notifications.
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  isFlutterLocalNotificationsInitialized = true;
 }
 
 FirebaseDatabase database = FirebaseDatabase.instance;
 DatabaseReference ref = FirebaseDatabase.instance.ref();
+String deviceTokenToSendPushNotification = "";
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -45,7 +91,137 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     readData();
+    getDeviceTokenToSendNotification();
+    firebaseInit();
     super.initState();
+  }
+  String? _token;
+  String? initialMessage;
+  bool _resolved = false;
+  firebaseInit(){
+        FirebaseMessaging.instance.getInitialMessage().then(
+          (value) => setState(
+            () {
+              _resolved = true;
+              initialMessage = value?.data.toString();
+            },
+          ),
+        );
+
+    FirebaseMessaging.onMessage.listen(showFlutterNotification);
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      // Navigator.pushNamed(
+      //   context,
+      //   '/message',
+      //   arguments: MessageArguments(message, true),
+      // );
+    });
+  }
+
+  Future<void> sendPushMessage() async {
+    if (_token == null) {
+      print('Unable to send FCM message, no token exists.');
+      return;
+    }
+
+    try {
+      await http.post(
+        Uri.parse('https://api.rnfirebase.io/messaging/send'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: constructFCMPayload(_token),
+      );
+      print('FCM request for device sent!');
+    } catch (e) {
+      print(e);
+    }
+  }
+  int _messageCount = 0;
+
+  String constructFCMPayload(String? token) {
+  _messageCount++;
+  return jsonEncode({
+    'token': token,
+    'data': {
+      'via': 'FlutterFire Cloud Messaging!!!',
+      'count': _messageCount.toString(),
+    },
+    'notification': {
+      'title': 'Hello FlutterFire!',
+      'body': 'This notification (#$_messageCount) was created via FCM!',
+    },
+  });
+}
+
+  Future<void> onActionSelected(String value) async {
+    switch (value) {
+      case 'subscribe':
+        {
+          print(
+            'FlutterFire Messaging Example: Subscribing to topic "fcm_test".',
+          );
+          await FirebaseMessaging.instance.subscribeToTopic('fcm_test');
+          print(
+            'FlutterFire Messaging Example: Subscribing to topic "fcm_test" successful.',
+          );
+        }
+        break;
+      case 'unsubscribe':
+        {
+          print(
+            'FlutterFire Messaging Example: Unsubscribing from topic "fcm_test".',
+          );
+          await FirebaseMessaging.instance.unsubscribeFromTopic('fcm_test');
+          print(
+            'FlutterFire Messaging Example: Unsubscribing from topic "fcm_test" successful.',
+          );
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+  void showFlutterNotification(RemoteMessage message) {
+  RemoteNotification? notification = message.notification;
+  AndroidNotification? android = message.notification?.android;
+  if (notification != null && android != null && !kIsWeb) {
+    flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          // TODO add a proper drawable resource to android, for now using
+          //      one that already exists in example app.
+          icon: 'launch_background',
+        ),
+      ),
+    );
+  }
+}
+
+  Future<void> getDeviceTokenToSendNotification() async {
+    final FirebaseMessaging _fcm = FirebaseMessaging.instance;
+    final token = await _fcm.getToken();
+    setState(() {
+      deviceTokenToSendPushNotification = token.toString();
+    });
+
+    sendTokenValue();
+    debugPrint("Token Value: $deviceTokenToSendPushNotification");
+  }
+
+  sendTokenValue() async {
+    await ref.update({
+      "app1Token": deviceTokenToSendPushNotification,
+    });
   }
 
   TextEditingController message = TextEditingController();
@@ -53,32 +229,38 @@ class _MyHomePageState extends State<MyHomePage> {
   readData() async {
     Stream<DatabaseEvent> stream = ref.onValue;
     stream.listen((DatabaseEvent event) {
-      print('Event Type: ${event.type}'); // DatabaseEventType.value;
-      print('Snapshot: ${event.snapshot.value}'); // DataSnapshot
-      if (event.snapshot.child("readmessage").value != "") {
-        setState(() {
-          messagetextfields.add(Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Align(
-              alignment: Alignment.topRight,
-              child:
-                  Text("Their :${event.snapshot.child("readmessage").value}"),
-            ),
-          ));
-        });
-        ref.update({
-          "readmessage": "",
-        });
-      }
-    });
-    setState(() {
-      data = "";
+      var d = const Duration(seconds: 2);
+      Future.delayed(d, () {
+        print('Event Type: ${event.type}');
+        print('Snapshot: ${event.snapshot.value}');
+        if (event.snapshot.child("sendmessage").value != "") {
+          setState(() {
+            messagetextfields.add(Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child:
+                    Text("Their :${event.snapshot.child("sendmessage").value}"),
+              ),
+            ));
+          });
+          ref.update({
+            "sendmessage": "",
+          });
+        }
+      });
+      setState(() {
+        data = "";
+      });
     });
   }
 
   void sendData(String value) async {
     await ref.update({
-      "sendmessage": value,
+      "readmessage": value,
+    });
+    await ref.update({
+      "readmessage": "",
     });
 
     setState(() {
@@ -121,6 +303,10 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         // mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Text("Your token $deviceTokenToSendPushNotification"),
+          // ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
